ba8ad3a6b07ad2ae3fd06f8e7ecc2229afdb33f2
rajitbanerjee <rajit.banerjee@ucdconnect.ie>
2020-01-27 22:33:04 +0000

    Added inner class Node

--- src/projectCode20280/SinglyLinkedList.java
+++ src/projectCode20280/SinglyLinkedList.java
@@ -4,12 +4,46 @@ import java.util.Iterator;
 
 public class SinglyLinkedList<E> implements List<E> {
 
-	private class Node<E> {
-		/// TODO
-	}
-	
-	@Override
-	public boolean isEmpty() {
+    // Inner class to describe the nodes that compose the list
+    private static class Node<E> {
+        // Instance variables to represent a Node element and a reference to the next Node
+        private E element;
+        private Node<E> next;
+
+        /**
+         * Creates a new node with given element and next node reference.
+         *
+         * @param element the element that will compose the list
+         * @param next    reference to the next node
+         */
+        public Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        //  Accessor for element
+        public E getElement() {
+            return element;
+        }
+
+        // Accessor for next Node<E>
+        public Node<E> getNext() {
+            return next;
+        }
+
+        // Mutator for element
+        public void setElement(E element) {
+            this.element = element;
+        }
+
+        // Mutator for next Node<E>
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
 		// TODO Auto-generated method stub
 		return false;
 	}
