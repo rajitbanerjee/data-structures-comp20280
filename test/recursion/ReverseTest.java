5560facde7441f5f9b1ecccb4cf04b6b97f2546d
rajitbanerjee <rajit.banerjee@ucdconnect.ie>
2020-02-23 01:22:59 +0000

    Recursive array reversal

---
+++ test/recursion/ReverseTest.java
@@ -0,0 +1,18 @@
+package recursion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseTest {
+    @Test
+    void testReverse() {
+        Integer[] A = {1, 2, 3, 4, 5};
+        Reverse.reverseArray(A, 0, A.length - 1);
+        int temp = 5;
+        for (int num : A) {
+            assertEquals(temp, num);
+            temp--;
+        }
+    }
+}
